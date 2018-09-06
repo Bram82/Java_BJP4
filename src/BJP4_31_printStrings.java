d29fbef7cddd75f08fb6e1ac297d47d4ff430050
Bram van Onna <bramvanonna@hotmail.com>
2018-09-06 22:22:19 +0200

    Second

---
+++ src/BJP4_31_printStrings.java
@@ -0,0 +1,12 @@
+public class BJP4_31_printStrings {
+    public static void main(String[] args) {
+        printStrings("abc", 5);
+    }
+
+    public static void printStrings(String input, Integer i) {
+        for (int x = 1; x <= i; x++) {
+            System.out.print(input);
+        }
+
+    }
+}
